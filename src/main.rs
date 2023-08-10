3fe9d50de04eb9d190e3f2f8919ad863d7ca631c
kaya <84478450+dvub@users.noreply.github.com>
2023-08-10 15:50:39 -0700

    add logic

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,60 @@ pub mod schema;
 pub mod ops;
 pub mod crypto;
 
-use diesel::SqliteConnection;
+use crypto::derive_and_encrypt;
 use ops::*;
-use schema::password::dsl::*;
 use args::PwdArgs;
 use clap::Parser;
 
-use crypto::hash;
+use ops::{check_master_exists, insert_master_password};
+
+
 
 fn main() {
     let args = PwdArgs::parse();
+    // create connection
+    println!("Retrieving database..");
     let mut conn = establish_connection();
-    match validate(&mut conn, args.master_password.as_bytes()) {
-        true => {
-            println!("validated!");
+    println!("Connected to database!\n");
+
+    println!("Checking for master record...");
+    let master_exists = check_master_exists(&mut conn);
+
+    match args.command {
+        args::PasswordCommands::Add {name, email, username, notes, password_type} => {
+            // 
+            if name == ops::MASTER_KEYWORD {
+                if master_exists {
+                    println!("Master record already exists.");
+                } else {
+                    insert_master_password(&mut conn, args.master_password.as_bytes());
+                    println!("Created new master record.");
+                }
+                return;
+            }
+
+            if !master_exists {
+                println!("Master record does not exist. Run 'pwd-rs -P <master_password> --name .master' to create a master record.");
+                return;
+            } 
+
+            println!("Found master record!");
+
+            if !authenticate(&mut conn, args.master_password.as_bytes()) {
+                println!("Incorrect master password.");
+                return;
+            }
+
+            println!("Successfully authenticated using master record.");
         }
-        false => {
-            println!("validation failed..");
+        args::PasswordCommands::Get { name } => {
+
         }
     }
+}
 
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    PwdArgs::command().debug_assert()
 }
