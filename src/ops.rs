4e670735b1bcc498b7328fbd0ea68cc4dd29525c
kaya <84478450+dvub@users.noreply.github.com>
2023-08-13 18:21:11 -0700

    new high level insert abstraction

--- src/ops.rs
+++ src/ops.rs
@@ -1,6 +1,8 @@
-use crate::crypto::{hash, derive_and_encrypt};
+use crate::crypto::{hash, encrypt_if_some};
 use crate::models::{NewPassword, Password, PasswordForm};
 use crate::schema::password::dsl::*;
+use aes_gcm::aead::OsRng;
+use aes_gcm::{Aes256Gcm, AeadCore};
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenvy::dotenv;
@@ -18,16 +20,17 @@ pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url).expect("Error connecting to database")
 }
-
 // insert password given an object of type NewPassword and a connection
 pub fn insert_password(
     connection: &mut SqliteConnection,
     new_password: NewPassword,
-) -> Result<usize, diesel::result::Error> {
+) {
     diesel::insert_into(password)
         .values(&new_password)
         .execute(connection)
+        .expect("error inserting password");
 }
+
 // get a password given a name of type &str and a connection
 pub fn get_password(connection: &mut SqliteConnection, term: &str) -> Option<Password> {
     password
@@ -69,31 +72,68 @@ pub fn insert_master_password(connection: &mut SqliteConnection, data: &[u8]) {
     let master_password = hash(data);
     let encoded = hex::encode(master_password);
     let data = Some(encoded.as_str());
-    insert_password(
-        connection,
-        NewPassword {
-            name: super::MASTER_KEYWORD,
+        diesel::insert_into(password)
+        .values(NewPassword {
+            name: MASTER_KEYWORD,
             username: None,
             email: None,
             pass: data,
             notes: None,
-            aes_nonce: None,
-        },
-    )
-    .unwrap();
+            aes_nonce: "",
+        })
+        .execute(connection)
+        .expect("error inserting master password");
 }
+
+pub fn encrypt_and_insert_password(
+    connection: &mut SqliteConnection,
+    master_password: &str,
+    new_name: &str,
+    new_username: Option<&str>,
+    new_email: Option<&str>,
+    new_pass: Option<&str>,
+    new_notes: Option<&str>,
+    
+) {
+
+    let nonce = Aes256Gcm::generate_nonce(OsRng);
+
+    let encrypted_username = encrypt_if_some(new_username, &master_password, nonce, new_name);
+    
+    let encrypted_email = encrypt_if_some(new_email, &master_password, nonce, &new_name);
+    let encrypted_pass = encrypt_if_some(new_pass, &master_password, nonce, &new_name);
+    let encrypted_notes = encrypt_if_some(new_notes, &master_password, nonce, &new_name);
+
+    let encoded_nonce = hex::encode(nonce);
+    let new_password = NewPassword {
+        name: new_name,
+        username: encrypted_username.as_deref(),
+        email: encrypted_email.as_deref(),
+        pass: encrypted_pass.as_deref(),
+        notes: encrypted_notes.as_deref(),
+        aes_nonce: &encoded_nonce,
+    };
+    insert_password(connection, new_password);
+
+}
+
 
 // test module for CRUD ops
 
 #[cfg(test)]
 mod tests {
     use super::{delete_password, get_password, insert_password, update_password};
-    use crate::crypto::hash;
+    use crate::crypto::generate_key;
     use crate::models::{NewPassword, PasswordForm};
     use crate::schema::password::dsl::*;
+    use aes_gcm::aead::Aead;
+    use aes_gcm::aead::consts::{B1, B0};
+    use aes_gcm::aead::generic_array::GenericArray;
+    use aes_gcm::{Aes256Gcm, KeyInit};
     use diesel::prelude::*;
     use diesel::{Connection, SqliteConnection};
     use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+    use sha2::digest::typenum::{UInt, UTerm};
 
     // the embed_migrations! macro will generate a constant value containing migrations, which are
     // stored in the binary
@@ -115,9 +155,9 @@ mod tests {
             email: Some("test@test.com"),
             pass: None,
             notes: None,
-            aes_nonce: None,
+            aes_nonce: "",
         };
-        insert_password(connection, new_password).unwrap();
+        insert_password(connection, new_password);
     }
 
     #[test]
@@ -158,7 +198,7 @@ mod tests {
                 email: None,
                 pass: None,
                 notes: None,
-                aes_nonce: None,
+                aes_nonce: "",
             },
         );
         let res = get_password(&mut conn, "foo");
@@ -176,4 +216,26 @@ mod tests {
         super::insert_master_password(&mut conn, b"mymasterpassword");
         assert!(!super::authenticate(&mut conn, b"randomguess"));
     }
+    #[test]
+    fn read_encrypted_data() {
+        let mut conn = establish_in_memory_connection();
+        super::encrypt_and_insert_password(&mut conn, "mymasterpassword", "tester", Some("test"), Some("tester@test.com"), Some("secret_pass123!"), None);
+
+        let key = generate_key(b"mymasterpassword", b"tester");
+        let cipher = Aes256Gcm::new(&key.into());
+
+        let res = get_password(&mut conn, "tester").unwrap();
+
+        let nonce = hex::decode(res.aes_nonce).unwrap();
+
+        let ciphertext = hex::decode(res.pass.expect("no password")).expect("error decoding password");
+
+        let plaintext = cipher.decrypt(GenericArray::from_slice(&nonce), ciphertext.as_ref()).expect("asd");
+        assert_eq!(plaintext, b"secret_pass123!");
+
+
+
+
+    }
+
 }
