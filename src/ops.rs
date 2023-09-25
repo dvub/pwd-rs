1a6e410aa37819797a34421271aaad1e2135acda
kaya <84478450+dvub@users.noreply.github.com>
2023-09-24 17:20:22 -0700

    refactor

--- src/ops.rs
+++ src/ops.rs
@@ -19,32 +19,37 @@ pub const MASTER_KEYWORD: &str = ".master";
 // these functions have a connection parameter so that a ":memory:" connection can be based for in-memory testing
 
 // simple function, returns SqliteConnection by reading address from .env
-pub fn establish_connection() -> SqliteConnection {
+pub fn establish_connection() -> Result<SqliteConnection, ConnectionError> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url).expect("Error connecting to database")
+    SqliteConnection::establish(&database_url)
 }
 // insert password given an object of type NewPassword and a connection
-pub fn insert_password(connection: &mut SqliteConnection, new_password: NewPassword) {
+pub fn insert_password(
+    connection: &mut SqliteConnection,
+    new_password: NewPassword,
+) -> Result<usize, diesel::result::Error> {
     diesel::insert_into(password)
         .values(&new_password)
         .execute(connection)
-        .expect("error inserting password");
 }
 
 // get a password given a name of type &str and a connection
-pub fn get_password(connection: &mut SqliteConnection, term: &str) -> Option<Password> {
+pub fn get_password(
+    connection: &mut SqliteConnection,
+    term: &str,
+) -> Result<Option<Password>, diesel::result::Error> {
     password
         .filter(name.eq(term))
         .select(Password::as_select())
         .first(connection)
         .optional()
-        .expect("error getting password")
 }
 // delete a password given a name, again, not generic.
 pub fn delete_password(connection: &mut SqliteConnection, term: &str) {
     let _ = diesel::delete(password.filter(name.eq(term))).execute(connection);
 }
+
 pub fn update_password(connection: &mut SqliteConnection, term: &str, form: PasswordForm) {
     let _ = diesel::update(password.filter(name.eq(term)))
         .set(form)
@@ -52,25 +57,43 @@ pub fn update_password(connection: &mut SqliteConnection, term: &str, form: Pass
         .expect("error updating password");
 }
 
-pub fn check_password_exists(connection: &mut SqliteConnection, term: &str) -> bool {
+pub fn check_password_exists(
+    connection: &mut SqliteConnection,
+    term: &str,
+) -> Result<bool, diesel::result::Error> {
     let res = get_password(connection, term);
     match res {
-        Some(_) => true,
-        None => false,
+        Ok(values) => match values {
+            Some(_) => Ok(true),
+            None => Ok(false),
+        },
+        Err(e) => Err(e),
     }
 }
 
-pub fn authenticate(connection: &mut SqliteConnection, master_password: &[u8]) -> bool {
-    let res =
-        get_password(connection, MASTER_KEYWORD).expect("Error: Master password does not exist");
-    hex::decode(
-        res.pass
-            .expect("Error: Master record exists but has no password"),
-    )
-    .unwrap()
-        == hash(master_password).to_vec()
+pub fn authenticate(
+    connection: &mut SqliteConnection,
+    master_password: &[u8],
+) -> Result<bool, diesel::result::Error> {
+    let res = get_password(connection, MASTER_KEYWORD);
+    match res {
+        Ok(v) => match v {
+            Some(value) => Ok(hex::decode(
+                value
+                    .pass
+                    .expect("Error: Master record exists but has no password"),
+            )
+            .expect("Error decoding")
+                == hash(master_password).to_vec()),
+            None => Err(diesel::result::Error::NotFound),
+        },
+        Err(e) => Err(e),
+    }
 }
-pub fn insert_master_password(connection: &mut SqliteConnection, data: &[u8]) {
+pub fn insert_master_password(
+    connection: &mut SqliteConnection,
+    data: &[u8],
+) -> Result<usize, diesel::result::Error> {
     let master_password = hash(data);
     let encoded = hex::encode(master_password);
     let data = Some(encoded.as_str());
@@ -84,7 +107,6 @@ pub fn insert_master_password(connection: &mut SqliteConnection, data: &[u8]) {
             aes_nonce: "",
         })
         .execute(connection)
-        .expect("error inserting master password");
 }
 // higher level functions::
 
@@ -112,13 +134,13 @@ pub fn encrypt_and_insert(
 
     let new_password = NewPassword {
         name: new_name,
-        username: encrypted_values.get(0).unwrap().as_deref(),
-        email: encrypted_values.get(1).unwrap().as_deref(),
-        pass: encrypted_values.get(2).unwrap().as_deref(),
-        notes: encrypted_values.get(3).unwrap().as_deref(),
+        username: encrypted_values[0].as_deref(),
+        email: encrypted_values[1].as_deref(),
+        pass: encrypted_values[2].as_deref(),
+        notes: encrypted_values[3].as_deref(),
         aes_nonce: &encoded_nonce,
     };
-    insert_password(connection, new_password);
+    let _ = insert_password(connection, new_password);
 }
 
 // this function will search by the term parameter for a password, and decrypt the fields if the password is found.
@@ -127,37 +149,45 @@ pub fn read_and_decrypt(
     connection: &mut SqliteConnection,
     master_password: &str,
     term: &str,
-) -> Option<Password> {
-    let res = get_password(connection, term);
-    match res {
-        Some(value) => {
-            let params = vec![value.username, value.email, value.pass, value.notes];
-            let mut decrypted = Vec::<Option<String>>::new();
-            for param in params {
-                decrypted.push(decrypt(
-                    master_password,
-                    param,
-                    &value.aes_nonce,
-                    &value.name,
-                ));
-            }
+) -> Result<Option<Password>, diesel::result::Error> {
+    let pwd = get_password(connection, term);
+    match pwd {
+        Ok(value) => {
+            match value {
+                Some(value) => {
+                    let params = vec![value.username, value.email, value.pass, value.notes];
+                    let mut decrypted = Vec::<Option<String>>::new();
+                    for param in params {
+                        decrypted.push(decrypt(
+                            master_password,
+                            param,
+                            &value.aes_nonce,
+                            &value.name,
+                        ));
+                    }
 
-            // clone() could be pretty inefficient in some cases, so this might have to be rewritten
-            // todo
-            // (?)
-            Some(Password {
-                id: value.id,
-                name: value.name,
-                username: decrypted.get(0).unwrap().clone(),
-                email: decrypted.get(1).unwrap().clone(),
-                pass: decrypted.get(2).unwrap().clone(),
-                notes: decrypted.get(3).unwrap().clone(),
-                aes_nonce: value.aes_nonce,
-            })
+                    // clone() could be pretty inefficient in some cases, so this might have to be rewritten
+                    // todo
+                    // (?)
+                    Ok(Some(Password {
+                        id: value.id,
+                        name: value.name,
+                        username: decrypted[0].clone(),
+                        email: decrypted[1].clone(),
+                        pass: decrypted[2].clone(),
+                        notes: decrypted[3].clone(),
+                        aes_nonce: value.aes_nonce,
+                    }))
+                }
+                None => Ok(None),
+            }
         }
-        None => None,
+        Err(e) => Err(e),
     }
 }
+
+// tests
+// thank god i can use unwrap or expect or whatever shit fuckery i want down here
 
 #[cfg(test)]
 mod tests {
@@ -173,8 +203,11 @@ mod tests {
     // testing only function that creates a new connection in memory,
     // and applies the migrations we generated from the embed_migrations! macro
     fn establish_in_memory_connection() -> SqliteConnection {
-        let mut connection = SqliteConnection::establish(":memory:").unwrap();
-        connection.run_pending_migrations(MIGRATIONS).unwrap();
+        let mut connection =
+            SqliteConnection::establish(":memory:").expect("error establishing connection");
+        connection
+            .run_pending_migrations(MIGRATIONS)
+            .expect("error running migrations");
         connection
     }
     // testing-only function that inserts 1 test record with some data into a table
@@ -190,7 +223,7 @@ mod tests {
             notes: None,
             aes_nonce: "",
         };
-        super::insert_password(connection, new_password);
+        let _ = super::insert_password(connection, new_password);
     }
 
     #[test]
@@ -206,7 +239,7 @@ mod tests {
         insert_test_data(&mut conn);
 
         let res = super::get_password(&mut conn, "test").unwrap();
-        assert_eq!(res.username, Some("tester".to_string()));
+        assert_eq!(res.unwrap().username, Some("tester".to_string()));
     }
     #[test]
     fn delete() {
@@ -236,19 +269,19 @@ mod tests {
             },
         );
         let res = super::get_password(&mut conn, "foo");
-        assert_eq!(res.unwrap().name, "foo");
+        assert_eq!(res.unwrap().unwrap().name, "foo");
     }
     #[test]
     fn authenticate() {
         let mut conn = establish_in_memory_connection();
-        super::insert_master_password(&mut conn, b"mymasterpassword");
-        assert!(super::authenticate(&mut conn, b"mymasterpassword"));
+        let _ = super::insert_master_password(&mut conn, b"mymasterpassword");
+        assert!(super::authenticate(&mut conn, b"mymasterpassword").unwrap());
     }
     #[test]
     fn failed_authentication() {
         let mut conn = establish_in_memory_connection();
-        super::insert_master_password(&mut conn, b"mymasterpassword");
-        assert!(!super::authenticate(&mut conn, b"randomguess"));
+        let _ = super::insert_master_password(&mut conn, b"mymasterpassword");
+        assert!(!super::authenticate(&mut conn, b"randomguess").unwrap());
     }
     #[test]
     fn encrypt_and_insert_password() {
@@ -303,6 +336,6 @@ mod tests {
             None,
         );
         let res = super::read_and_decrypt(&mut conn, "mymasterpassword", "salt");
-        assert_eq!(res.unwrap().username.unwrap(), "tester1");
+        assert_eq!(res.unwrap().unwrap().username.unwrap(), "tester1");
     }
 }
