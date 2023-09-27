e3325e4446f505bc401c8d3c8ff7babacbaf1ed9
kaya <84478450+dvub@users.noreply.github.com>
2023-09-26 18:47:10 -0700

    work on output messages

--- src/console.rs
+++ src/console.rs
@@ -1,9 +1,12 @@
+use std::io::Write;
+
 use colored::Colorize;
 pub fn checking(message: &str) {
-    println!("{}: {}", "checking".yellow().bold(), message);
+    print!("{}: {}", "checking".yellow().bold(), message);
+    std::io::stdout().flush().unwrap();
 }
 pub fn success(message: &str) {
-    println!("{}: {}", "success".green().bold(), message);
+    println!("\x1B[1A\x1B[K{}: {}", "success".green().bold(), message);
 }
 pub fn error(message: &str) {
     println!("{}: {}", "error".red().bold(), message);
