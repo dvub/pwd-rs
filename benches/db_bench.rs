9bb742d1b25a4f2541ba324892c72c309026dd98
dvub <dvubdevs@gmail.com>
2023-10-24 21:58:17 -0700

    Work on testing

--- benches/db_bench.rs
+++ benches/db_bench.rs
@@ -1,7 +1,24 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use pwd_rs::{
+    models::NewPassword,
+    ops::{encrypt_and_insert, establish_connection},
+};
 pub fn criterion_benchmark(c: &mut Criterion) {
-    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    let mut connection = establish_connection().unwrap();
+
+    c.bench_function("fib 20", |b| {
+        b.iter(|| {
+            encrypt_and_insert(
+                &mut connection,
+                black_box("mymasterpassword"),
+                black_box("test_info"),
+                black_box(Some("user123".to_string())),
+                black_box(Some("tester@test.com".to_string())),
+                black_box(Some("mycoolpassword".to_string())),
+                black_box(Some("some notes".to_string())),
+            )
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
